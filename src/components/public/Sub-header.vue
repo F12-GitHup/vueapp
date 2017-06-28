8db670bd4e120eb5819772fef2ed68c8db8d219e
F12-GitHup <3066123223@qq.cpm>
2017-06-28 10:01:36 +0800

    lidejv6

--- src/components/public/Sub-header.vue
+++ src/components/public/Sub-header.vue
@@ -1,17 +1,30 @@
 <template>  
    <div class="sub-header">
         <ul class="class-list">
-            <li>1</li>
+            <li v-for="(ele,index) in classData" key="index">
+                {{ele.title}}
+            </li>
+            
         </ul>
    </div>
 </template>
 
 <script>
     export default({
-        name:"sub-header"
+        name:"sub-header",
+        props:{
+            classData:{
+                type:Array,
+                default(){
+                    return []
+                }
+            }
+        }
     })
 </script>
 
 <style scoped>
-   .sub-header{width:100%;height:42px;background:#ccc;position:fixed;top:45px;left:0;}
+   .sub-header{width:100%;height:42px;background:#fff;position:fixed;top:45px;left:0;z-index:5;}
+    .class-list{width:100%;display:flex;line-height:42px;overflow-x:scroll;white-space:nowrap;}
+  .class-list li{padding:0 10px;color:#f10;}
 </style>
